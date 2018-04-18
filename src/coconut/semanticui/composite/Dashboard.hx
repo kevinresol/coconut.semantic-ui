c8e60a9e8f0ac55bf99bd9dc414239bb26d1d509
Kevin Leung <kevinresol@gmail.com>
2018-04-18 22:22:29 +0800

    Update Dashboard: rename renderSidebar to sidebar

--- src/coconut/semanticui/composite/Dashboard.hx
+++ src/coconut/semanticui/composite/Dashboard.hx
@@ -4,21 +4,31 @@ import coconut.ui.*;
 import tink.pure.*;
 import js.jquery.Helper.*;
 
+/**
+ *  Example:
+ *  <Dashboard>
+ *  	<sidebar attr><Sidebar app=${app} ${...attr}/></renderSidebar>
+ *  	<navbar><NavBar app=${app}/></navbar>
+ *  	<content><AppContent app=${app}/></content>
+ *  	<footer><Footer/></footer>
+ *  </Dashboard>
+ */
+
 @:less('dashboard.less')
 class Dashboard extends coconut.ui.View {
-	@:attribute var renderSidebar:{className:String}->RenderResult;
+	@:attribute var sidebar:{className:String}->RenderResult;
 	@:attribute var navbar:RenderResult;
 	@:attribute var content:RenderResult;
 	@:attribute var footer:RenderResult;
 
 	function render() '
 		<div class="pushable">
-			<renderSidebar class="ui sidebar inverted vertical menu"/>
+			<sidebar class="ui sidebar inverted vertical menu"/>
 			<div class="pusher">
 				<div>
 					<div class="full height">
 						<div class="toc">
-							<renderSidebar class="ui inverted attached vertical menu"/>
+							<sidebar class="ui inverted attached vertical menu"/>
 						</div>
 						<div class="main">
 							${navbar}
