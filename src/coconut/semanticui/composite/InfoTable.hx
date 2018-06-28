1185b104f8e8680e6fe2da444602dde1adec0d98
Kevin Leung <kevinresol@gmail.com>
2018-06-28 13:44:50 +0800

    [InfoTable] Support links

--- src/coconut/semanticui/composite/InfoTable.hx
+++ src/coconut/semanticui/composite/InfoTable.hx
@@ -5,15 +5,27 @@ import tink.pure.List;
 using tink.CoreApi;
 
 class InfoTable extends coconut.ui.View {
-	@:attribute var entries:List<Named<String>>;
+	@:attribute var entries:List<Value>;
 	function render() '
 		<table class="ui unstackable very basic table">
 			<for ${entry in entries}>
 				<tr>
-					<td class="collapsing"><strong>${entry.name}</strong></td>
-					<td>${entry.value}</td>
+					<td class="collapsing"><strong>${entry.title}</strong></td>
+					<td>
+						<if ${entry.href == null}>
+							${entry.value}
+						<else>
+							<a href=${entry.href}>${entry.value}</a>
+						</if>
+					</td>
 				</tr>
 			</for>
 		</table>
 	';
+}
+
+private typedef Value = {
+	var title(default, never):String;
+	var value(default, never):String;
+	@:optional var href(default, never):String;
 }
