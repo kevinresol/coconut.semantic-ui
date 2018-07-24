3c840f09e344ebaa254ab06a20cd38e50bfb05e7
Kevin Leung <kevinresol@gmail.com>
2018-07-24 16:27:50 +0800

    Add ErrorMessage

---
+++ src/coconut/semanticui/composite/ErrorMessage.hx
@@ -0,0 +1,24 @@
+package coconut.semanticui.composite;
+
+using tink.CoreApi;
+
+class ErrorMessage extends coconut.ui.View {
+	@:attr var error:Error;
+	@:attr var title:String = null;
+	
+	
+	function render() '
+		<div class="ui error message">
+			<div class="content">
+				<if ${title != null}>
+					<div class="header">$title</div>
+				</if>
+				<p>
+					<i class="warning icon"/>
+					${error.message}
+					<if ${error.data != null}><input type="hidden" value=${Std.string(error.data)}/></if>
+				</p>
+			</div>
+		</div>
+	';
+}
