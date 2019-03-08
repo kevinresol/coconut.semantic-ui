b6033278602df2e5d42b8e6f26a5405d4100b8ca
Kevin Leung <kevinresol@gmail.com>
2019-03-08 17:31:57 +0800

    Add tbody

--- src/coconut/semanticui/composite/InfoTable.hx
+++ src/coconut/semanticui/composite/InfoTable.hx
@@ -8,18 +8,20 @@ class InfoTable extends coconut.ui.View {
 	@:attribute var entries:List<Value>;
 	function render() '
 		<table class="ui unstackable very basic table">
-			<for ${entry in entries}>
-				<tr>
-					<td class="collapsing"><strong>${entry.title}</strong></td>
-					<td style=${{whiteSpace:'pre-wrap'}}>
-						<if ${entry.href == null}>
-							${entry.value}
-						<else>
-							<a href=${entry.href}>${entry.value}</a>
-						</if>
-					</td>
-				</tr>
-			</for>
+			<tbody>
+				<for ${entry in entries}>
+					<tr>
+						<td class="collapsing"><strong>${entry.title}</strong></td>
+						<td style=${{whiteSpace:'pre-wrap'}}>
+							<if ${entry.href == null}>
+								${entry.value}
+							<else>
+								<a href=${entry.href}>${entry.value}</a>
+							</if>
+						</td>
+					</tr>
+				</for>
+			</tbody>
 		</table>
 	';
 }
